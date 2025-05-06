0658dfb600bea6aeafee0a1c75da95003e385a85
GMOfreek <nicklejamsey@gmail.com>
2025-05-06 16:04:21 -0700

    finished in 1 sitting

---
+++ src/cell.py
@@ -0,0 +1,61 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, win=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.visited = False
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
+
+    def draw_move(self, to_cell, undo=False):
+        current_x = self._x1 + (abs(self._x2 - self._x1) // 2)
+        current_y = self._y1 + (abs(self._x2 - self._x1) // 2)
+        future_x = to_cell._x1 + (abs(to_cell._x2 - to_cell._x1) // 2)
+        future_y = to_cell._y1 + (abs(to_cell._x2 - to_cell._x1) // 2)
+
+        line_color = "red"
+        if undo:
+            line_color = "gray"
+
+        line = Line(Point(current_x, current_y), Point(future_x, future_y))
+        self._win.draw_line(line, line_color)
